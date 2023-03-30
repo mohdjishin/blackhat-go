0cc3d57b301a86e56c9c22222eef32676aa69222
mohdjishin <whoistcp@protonmail.com>
2023-03-31 01:28:15 +0530

    concurrency port scanner

--- chap-01/main.go
+++ chap-01/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			// port is closed or filtered.
-			continue
-		}
-		conn.Close()
-		fmt.Printf("%d open\n", i)
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
 	}
+	wg.Wait()
 }
