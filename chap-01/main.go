a80c17fa8a7cc602235d4d4be83a4cf7421cc731
mohdjishin <whoistcp@protonmail.com>
2023-03-31 01:25:59 +0530

    port scanner

--- chap-01/main.go
+++ chap-01/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-		fmt.Println("Connection successful")
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			// port is closed or filtered.
+			continue
+		}
+		conn.Close()
+		fmt.Printf("%d open\n", i)
 	}
 }
